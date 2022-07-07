7bcb7b446e0933a21b8fb4b48024bf1235cf2776
Mateie <mateie@email.com>
2022-07-06 18:43:39 -0700

    updated link for logging in

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -44,7 +44,7 @@ const Navigation = () => {
         }
     ];
 
-    const authLink = 'https://discord.com/api/oauth2/authorize?client_id=969414951292788766&redirect_uri=http%3A%2F%2F73.185.96.104%3A3000%2Flogin&response_type=code&scope=identify%20guilds';
+    const authLink = process.env.NODE_ENV === 'production' ? 'https://discord.com/api/oauth2/authorize?client_id=969414951292788766&redirect_uri=http%3A%2F%2Fkurama.mateie.com%2Flogin&response_type=code&scope=identify%20guilds' : 'https://discord.com/api/oauth2/authorize?client_id=969414951292788766&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify%20guilds';
 
     if (!auth) return <Menubar className='top-nav' model={items} end={<Button label='Login' className='p-button-success' onClick={() => window.location.href = authLink} />} />;
 
