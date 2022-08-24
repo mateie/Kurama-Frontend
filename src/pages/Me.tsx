f5c84b82df92dcad66481d457f6fedfdbab79f6d
Mateie <mateie@email.com>
2022-08-24 01:48:45 -0700

    navigate back to home when no user

--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -2,9 +2,12 @@ import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 
 import UserRankImage from "../components/User/UserRankImage";
+import { Navigate } from "react-router-dom";
 
 const Me = () => {
     const { auth }: { auth: any } = useContext(AuthContext);
+
+    if (!auth) return <Navigate to='/' replace={true} />
 
     return (
         <div className="flex justify-content-between align-items-center">
