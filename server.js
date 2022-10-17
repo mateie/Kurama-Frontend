90476d8dfcfa9580f214125d0b279b8f6bc6a1c5
Mateie <kurama@mateie.com>
2022-10-17 00:22:03 -0700

    made port default to 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 80;
 const publicPath = path.join(__dirname, "build");
 
 app.use(express.static(publicPath));
