a890fb46fdf12487f7352583e48670b25321edd6
Mateie <kurama@mateie.com>
2022-10-17 00:12:59 -0700

    changed default port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 const publicPath = path.join(__dirname, "build");
 
 app.use(express.static(publicPath));
