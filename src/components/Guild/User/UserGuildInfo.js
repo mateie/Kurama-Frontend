108f4cadbf23214fd9fd0139835873fe2d5cb54e
Mateie <kurama@mateie.com>
2022-10-18 16:45:37 -0700

    fixed an owner bug

--- src/components/Guild/User/UserGuildInfo.js
+++ src/components/Guild/User/UserGuildInfo.js
@@ -1,19 +1,22 @@
-import { useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 
 import { Button } from "primereact/button";
 import { Chip } from "primereact/chip";
 
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
+
 import { FETCH_USER } from "../../../gql/queries/users";
 
 const UserGuildInfo = ({ guild }) => {
     const navigate = useNavigate();
+    const { auth } = useContext(AuthContext);
+    const [fetchOwner, { loading, data: { user: owner } = {} }] = useLazyQuery(FETCH_USER);
 
-    const { loading, data: { user: owner } = {} } = useQuery(FETCH_USER, {
-        variables: {
-            userId: guild.ownerId
-        }
-    });
+    useEffect(() => {
+        if(guild.ownerId) fetchOwner({ variables: { userId: guild.ownerId } });
+    }, [guild, guild.ownerId]);
 
     const avatar = loading ? (
         "Loading..."
@@ -27,21 +30,12 @@ const UserGuildInfo = ({ guild }) => {
         <div className="guild">
             <div className="guild-content">
                 <div className="mb-3">
-                    {guild.botJoined ? (
-                        <img
-                            src={guild.iconURL}
-                            className="guild-icon"
-                            id="joined"
-                            alt="Guild Icon"
-                            onClick={() => navigate(`/guild/${guild.id}`)}
-                        />
-                    ) : (
-                        <img
-                            src={guild.iconURL}
-                            className="guild-icon"
-                            alt="Guild Icon"
-                        />
-                    )}
+                    <img
+                        src={guild.iconURL}
+                        className="guild-icon"
+                        alt="Guild Icon"
+                        onClick={() => navigate(`/guild/${guild.id}`)}
+                    />
                 </div>
                 <div>
                     <h3 className="mb-1">{guild.name}</h3>
@@ -54,19 +48,25 @@ const UserGuildInfo = ({ guild }) => {
                     </h4>
                     <h5 className="mb-3">{avatar}</h5>
                     <h6 className="mt-0 mb-3">
-                        {guild.botJoined ? guild.members.length : "Unknown"}{" "}
-                        Members
+                        {guild.members ? guild.members.length : "Unknown"} Members
                     </h6>
-                    {!guild.botJoined && (
+                </div>
+                {guild.promoted &&
+                    (guild.members.includes(auth.id) ? (
                         <Button
-                            label="Add to the server"
+                            className="p-button-danger"
+                            label="Already joined"
+                            disabled
+                        />
+                    ) : (
+                        <Button
+                            className="p-button-success"
+                            label="Join the server"
                             onClick={() =>
-                                (window.location.href =
-                                    "https://discord.com/api/oauth2/authorize?client_id=969414951292788766&permissions=1644971949559&scope=applications.commands%20bot")
+                                window.open(guild.inviteURL, "_blank")
                             }
                         />
-                    )}
-                </div>
+                    ))}
             </div>
         </div>
     );
