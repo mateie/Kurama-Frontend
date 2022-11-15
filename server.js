a986041b7202cf74e0bfd1c0fa7af9ebf561d24e
Mateie <kurama@mateie.com>
2022-11-14 18:05:39 -0800

    forgot line of code lmao

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const http = require('http').createServer(app);
 const port = process.env.PORT || 80;
 const publicPath = path.join(__dirname, "build");
 
+app.use(express.static(publicPath));
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
 });
