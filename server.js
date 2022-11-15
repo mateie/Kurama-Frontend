b826bc9d966399738243c81cbfea6b47ec820fd6
Mateie <kurama@mateie.com>
2022-11-14 18:00:03 -0800

    changed up the server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
+
+const http = require('http').createServer(app);
 
 const port = process.env.PORT || 80;
 const publicPath = path.join(__dirname, "build");
-
-app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+http.listen(port, '0.0.0.0', () => {
+    console.log(`Listening to port: ${port}`);
 });
