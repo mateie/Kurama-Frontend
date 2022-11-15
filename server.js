98e034701506e6c06e0e3187ae4112bf4875e545
Mateie <kurama@mateie.com>
2022-11-14 18:38:08 -0800

    updated server,js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require('path');
 const app = express();
 
 let server = process.env.NODE_ENV !== 'development' ? require('https').createServer({
-    privateKey: fs.readFileSync('private.key'),
-    certificate: fs.readFileSync('public.cer')
+    privateKey: fs.readFileSync('private.key', 'utf-8'),
+    certificate: fs.readFileSync('public.cer', 'utf-8')
 }, app) : require('http').createServer(app);
 
 const port = process.env.NODE_ENV === 'development' ? 80 : 443;
@@ -18,6 +18,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
 });
 
-server.listen(port, '0.0.0.0', () => {
+server.listen(port, () => {
     console.log(`Listening to port: ${port}`);
 });
